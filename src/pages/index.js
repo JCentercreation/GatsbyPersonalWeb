6b7c7c695257c7122d0e0b13d71128d7ae92ffc1
Javier Carrillo Gallego <javiercarrillogallego@MacBook-Air-de-Javier.local>
2020-12-24 21:36:02 +0100

    Actualizacion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{color: 'red', backgroundColor: 'blue'}}>
+  return <div style={{color: 'red', backgroundColor: 'blue'}}> {/*Estamos trabajando en lenguaje JavaScrip X (JSX), que es parecido a CSS y HTML pero con particularidades. Como vemos hasta los comentarios se definen entre corchetes */}
             <h1>Ey, you dit it!</h1>
           </div>
 }
