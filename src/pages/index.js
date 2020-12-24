ca749f5ffecc219d14008802aef490d1993206be
Javier Carrillo Gallego <javiercarrillogallego@MacBook-Air-de-Javier.local>
2020-12-24 21:24:44 +0100

    Primer Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{color: 'red', backgroundColor: 'blue'}}>
+            <h1>Ey, you dit it!</h1>
+          </div>
 }
